82e2bdbe003f81c6762ab56c48fe86d4b56862be
t001978 <41689042+andlima72@users.noreply.github.com>
2022-09-02 22:13:11 -0300

    atualizar database dos arquivos dump

--- challenge_AluraCash/Dados/emprestimos.sql
+++ challenge_AluraCash/Dados/emprestimos.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
--- Host: localhost    Database: analise_risco
+-- Host: localhost    Database: alura
 -- ------------------------------------------------------
 -- Server version	8.0.30
 
